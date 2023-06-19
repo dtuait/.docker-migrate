5f25ab1ac15be7412b143b2b096afe1fdaaffdac
Victor Reipur <victor.reipur@gmail.com>
2023-06-19 14:40:34 +0200

    denne version virker

--- migrate.sh
+++ migrate.sh
@@ -11,31 +11,128 @@ while [ $# -gt 0 ]; do
 done
 
 
-if [[ -z "$migrate" ]]; then echo "Please specify '--migrate 'export|import''"; exit 1; fi;
+
+
+# if migrate is not not "export" or "import"
+# if [[ -z "$migrate" ]]; then echo "Please specify '--migrate 'export|import''"; exit 1; fi;
+if [[ "$migrate" != "export" && "$migrate" != "import" ]]; then echo "Please specify '--migrate 'export|import''"; exit 1; fi;
+
+# before doing anything make sure all containers are stopped
+docker stop $(docker ps -a -q) > /dev/null 2>&1
 
 # array of image names to import
-images=(
-    "web-django-6-django-app"
-    "web-django-6-webserver"
-)
+mapfile -t images < images.txt
 
-# Array of volume names to backup
-volumes=(
-    "django-app_webserver_var-www"
-    "django-app_webserver_etc-apache2"
-    "django-app_webserver_etc_letsencrypt-cached"
-    "django-app_django-app_usr-src-app"
-)
+# Read volumes.txt into array
+mapfile -t volumes < volumes.txt
 
 
-# if [ "$migrate" == "export" ]; then
-#     echo "Exporting volumes..."
-#     bash _export.sh
-#     echo "Done exporting volumes."
-# elif [ "$migrate" == "import" ]; then
-#     echo "Importing volumes..."
-#     bash _import.sh
-#     echo "Done importing volumes."
-# else
-#     echo "Please specify '--migrate 'export|import''"
-# fi
+
+# Export script starts here
+if [ "$migrate" == "export" ]; then
+
+    echo "Exporting volumes..."
+
+
+    # Delete the volumes directory if it exists, then recreate it
+    if [ -d "_volumes" ]; then
+        rm -rf _volumes
+    fi
+    mkdir -p ./_volumes
+
+    # Loop over volumes and back each one up
+    for volume in "${volumes[@]}"; do
+        echo "Backing up $volume..."
+        docker run --rm -v $volume:/volume -v $(pwd)/_volumes:/backup alpine tar -czvf /backup/$volume.tar /volume > /dev/null
+
+        if [ $? -eq 0 ]
+        then
+            echo "$volume backed up successfully."
+        else
+            echo "Error backing up $volume."
+            exit 1
+        fi
+    done
+
+    echo "Done exporting volumes."
+
+
+    echo "Exporting images..."
+    # Delete the images directory if it exists, then recreate it
+    if [ -d "_images" ]; then
+        rm -rf _images
+    fi
+        mkdir _images
+
+    for images in "${images[@]}"; do
+    safe_image_name=$(echo $images | tr '/' '.')
+    echo "Backing up $images..."
+    docker save -o _images/$safe_image_name.tar $images > /dev/null
+
+    if [ $? -eq 0 ]
+    then
+        echo "$images backed up successfully."
+    else
+        echo "Error backing up $images."
+        exit 1
+    fi
+
+    echo "Done exporting images."
+
+done
+
+# Export script ends here
+elif [ "$migrate" == "import" ]; then
+# Import script starts here
+    echo "Importing volumes..."
+    # import volumes to docker
+    for volume in "${volumes[@]}"; do
+
+        # Check if the volume exists, if not create it
+        if [ -z "$(docker volume ls -q | grep $volume)" ]; then
+
+            echo "Creating volume $volume..."
+            
+            docker volume create $volume
+
+            echo "Importing $volume..."
+            docker run --rm -v $volume:/volume -v $(pwd)/_volumes:/backup alpine tar -xzvf /backup/$volume.tar >/dev/null
+
+            if [ $? -eq 0 ]; then
+                echo "$volume imported successfully."
+            else
+                echo "Error importing $volume."
+                exit 1
+            fi
+
+        else
+            echo "Volume $volume already exists."
+            exit 1
+        fi
+
+    done
+
+    echo "Done importing volumes."
+
+    echo "Importing images..."
+    for image in "${images[@]}"; do
+        echo "Loading image $image..."
+        safe_image_name=$(echo $images | tr '/' '.')
+        docker load -i _images/$safe_image_name.tar >/dev/null
+
+        if [ $? -eq 0 ]; then
+            echo "$image loaded successfully."
+        else
+            echo "Error loading $image."
+            exit 1
+        fi
+    done
+
+    echo "Done importing images."
+
+# Import script ends here    
+fi
+
+
+echo "Script has completed $migrate"
+exit 0
--- readme.txt
+++ readme.txt
@@ -1,3 +1,12 @@
 In the migrate.sh file you specify the relevant volumes and images you want to export
 
 
+
+in your terminal type 'docker image ls'
+
+vicmrp@BYG-A209-VICRE:~/www/temp/docker-migrate$ docker image ls
+REPOSITORY                                                                        TAG       IMAGE ID       CREATED        SIZE
+vicmrp/django-app-webserver                                                       latest    061a0a29c049   23 hours ago   581MB
+vicmrp/django-app_django-app                                                      latest    967bbe9f5557   2 days ago     1.58GB
+
+
